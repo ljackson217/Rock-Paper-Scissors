a447c768701e20fd12fc41d26c89b743b4bac82a
ljackson217 <ljackson21790@gmail.com>
2021-07-21 14:58:28 -0700

    Add project files.

---
+++ Final Project/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿
+namespace Final_Project
+{
+    partial class rockPaperScissorsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.compChoiceLabel = new System.Windows.Forms.Label();
+            this.computerChoice = new System.Windows.Forms.Label();
+            this.yourChoiceLabel = new System.Windows.Forms.Label();
+            this.yourChoice = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.yourScoreLabel = new System.Windows.Forms.Label();
+            this.yourScoreOutput = new System.Windows.Forms.Label();
+            this.computerScoreLabel = new System.Windows.Forms.Label();
+            this.computerScoreOutput = new System.Windows.Forms.Label();
+            this.makeSelectionLabel = new System.Windows.Forms.Label();
+            this.rockRadioButton = new System.Windows.Forms.RadioButton();
+            this.paperRadioButton = new System.Windows.Forms.RadioButton();
+            this.scissorsRadioButton = new System.Windows.Forms.RadioButton();
+            this.goButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // compChoiceLabel
+            // 
+            this.compChoiceLabel.AutoSize = true;
+            this.compChoiceLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.compChoiceLabel.Location = new System.Drawing.Point(165, 94);
+            this.compChoiceLabel.Name = "compChoiceLabel";
+            this.compChoiceLabel.Size = new System.Drawing.Size(147, 20);
+            this.compChoiceLabel.TabIndex = 0;
+            this.compChoiceLabel.Text = "Computer\'s Choice:";
+            this.compChoiceLabel.Visible = false;
+            // 
+            // computerChoice
+            // 
+            this.computerChoice.AutoSize = true;
+            this.computerChoice.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.computerChoice.Location = new System.Drawing.Point(189, 138);
+            this.computerChoice.Name = "computerChoice";
+            this.computerChoice.Size = new System.Drawing.Size(99, 25);
+            this.computerChoice.TabIndex = 1;
+            this.computerChoice.Text = "scissors";
+            this.computerChoice.Visible = false;
+            // 
+            // yourChoiceLabel
+            // 
+            this.yourChoiceLabel.AutoSize = true;
+            this.yourChoiceLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.yourChoiceLabel.Location = new System.Drawing.Point(188, 211);
+            this.yourChoiceLabel.Name = "yourChoiceLabel";
+            this.yourChoiceLabel.Size = new System.Drawing.Size(100, 20);
+            this.yourChoiceLabel.TabIndex = 2;
+            this.yourChoiceLabel.Text = "Your Choice:";
+            this.yourChoiceLabel.Visible = false;
+            // 
+            // yourChoice
+            // 
+            this.yourChoice.AutoSize = true;
+            this.yourChoice.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.yourChoice.Location = new System.Drawing.Point(189, 258);
+            this.yourChoice.Name = "yourChoice";
+            this.yourChoice.Size = new System.Drawing.Size(99, 25);
+            this.yourChoice.TabIndex = 3;
+            this.yourChoice.Text = "scissors";
+            this.yourChoice.Visible = false;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(251, 381);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(94, 37);
+            this.exitButton.TabIndex = 7;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // yourScoreLabel
+            // 
+            this.yourScoreLabel.AutoSize = true;
+            this.yourScoreLabel.Location = new System.Drawing.Point(47, 53);
+            this.yourScoreLabel.Name = "yourScoreLabel";
+            this.yourScoreLabel.Size = new System.Drawing.Size(63, 13);
+            this.yourScoreLabel.TabIndex = 8;
+            this.yourScoreLabel.Text = "Your Score:";
+            // 
+            // yourScoreOutput
+            // 
+            this.yourScoreOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.yourScoreOutput.Location = new System.Drawing.Point(116, 52);
+            this.yourScoreOutput.Name = "yourScoreOutput";
+            this.yourScoreOutput.Size = new System.Drawing.Size(23, 19);
+            this.yourScoreOutput.TabIndex = 9;
+            // 
+            // computerScoreLabel
+            // 
+            this.computerScoreLabel.AutoSize = true;
+            this.computerScoreLabel.Location = new System.Drawing.Point(308, 53);
+            this.computerScoreLabel.Name = "computerScoreLabel";
+            this.computerScoreLabel.Size = new System.Drawing.Size(93, 13);
+            this.computerScoreLabel.TabIndex = 10;
+            this.computerScoreLabel.Text = "Computer\'s Score:";
+            // 
+            // computerScoreOutput
+            // 
+            this.computerScoreOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.computerScoreOutput.Location = new System.Drawing.Point(407, 52);
+            this.computerScoreOutput.Name = "computerScoreOutput";
+            this.computerScoreOutput.Size = new System.Drawing.Size(23, 19);
+            this.computerScoreOutput.TabIndex = 11;
+            // 
+            // makeSelectionLabel
+            // 
+            this.makeSelectionLabel.AutoSize = true;
+            this.makeSelectionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.makeSelectionLabel.Location = new System.Drawing.Point(129, 217);
+            this.makeSelectionLabel.Name = "makeSelectionLabel";
+            this.makeSelectionLabel.Size = new System.Drawing.Size(218, 24);
+            this.makeSelectionLabel.TabIndex = 12;
+            this.makeSelectionLabel.Text = "Please make a selection:";
+            // 
+            // rockRadioButton
+            // 
+            this.rockRadioButton.AutoSize = true;
+            this.rockRadioButton.Checked = true;
+            this.rockRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rockRadioButton.Location = new System.Drawing.Point(104, 316);
+            this.rockRadioButton.Name = "rockRadioButton";
+            this.rockRadioButton.Size = new System.Drawing.Size(64, 24);
+            this.rockRadioButton.TabIndex = 13;
+            this.rockRadioButton.TabStop = true;
+            this.rockRadioButton.Text = "Rock";
+            this.rockRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // paperRadioButton
+            // 
+            this.paperRadioButton.AutoSize = true;
+            this.paperRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.paperRadioButton.Location = new System.Drawing.Point(192, 316);
+            this.paperRadioButton.Name = "paperRadioButton";
+            this.paperRadioButton.Size = new System.Drawing.Size(69, 24);
+            this.paperRadioButton.TabIndex = 14;
+            this.paperRadioButton.Text = "Paper";
+            this.paperRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // scissorsRadioButton
+            // 
+            this.scissorsRadioButton.AutoSize = true;
+            this.scissorsRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scissorsRadioButton.Location = new System.Drawing.Point(285, 316);
+            this.scissorsRadioButton.Name = "scissorsRadioButton";
+            this.scissorsRadioButton.Size = new System.Drawing.Size(87, 24);
+            this.scissorsRadioButton.TabIndex = 15;
+            this.scissorsRadioButton.Text = "Scissors";
+            this.scissorsRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // goButton
+            // 
+            this.goButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.goButton.Location = new System.Drawing.Point(131, 381);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(94, 37);
+            this.goButton.TabIndex = 16;
+            this.goButton.Text = "Go!";
+            this.goButton.UseVisualStyleBackColor = true;
+            this.goButton.Click += new System.EventHandler(this.goButton_Click);
+            // 
+            // rockPaperScissorsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 470);
+            this.Controls.Add(this.goButton);
+            this.Controls.Add(this.scissorsRadioButton);
+            this.Controls.Add(this.paperRadioButton);
+            this.Controls.Add(this.rockRadioButton);
+            this.Controls.Add(this.makeSelectionLabel);
+            this.Controls.Add(this.computerScoreOutput);
+            this.Controls.Add(this.computerScoreLabel);
+            this.Controls.Add(this.yourScoreOutput);
+            this.Controls.Add(this.yourScoreLabel);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.yourChoice);
+            this.Controls.Add(this.yourChoiceLabel);
+            this.Controls.Add(this.computerChoice);
+            this.Controls.Add(this.compChoiceLabel);
+            this.Name = "rockPaperScissorsForm";
+            this.Text = "Rock, Paper, Scissors";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label compChoiceLabel;
+        private System.Windows.Forms.Label computerChoice;
+        private System.Windows.Forms.Label yourChoiceLabel;
+        private System.Windows.Forms.Label yourChoice;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label yourScoreLabel;
+        private System.Windows.Forms.Label yourScoreOutput;
+        private System.Windows.Forms.Label computerScoreLabel;
+        private System.Windows.Forms.Label computerScoreOutput;
+        private System.Windows.Forms.Label makeSelectionLabel;
+        private System.Windows.Forms.RadioButton rockRadioButton;
+        private System.Windows.Forms.RadioButton paperRadioButton;
+        private System.Windows.Forms.RadioButton scissorsRadioButton;
+        private System.Windows.Forms.Button goButton;
+    }
+}
+
---
+++ Final Project/Form1.cs
@@ -0,0 +1,268 @@
+﻿// Lindsay Allen (Jackson)
+// CIS162AD - #31778
+// Final Project
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Final_Project
+{
+    public partial class rockPaperScissorsForm : Form
+    {
+        public rockPaperScissorsForm()
+        {
+            InitializeComponent();
+        }
+
+        // initializes variables
+        Random rand = new Random(); // creates a new random object
+        string[] compChoices = { "Rock", "Paper", "Scissors" }; // array of selections for the computer
+        int compChoiceIndex;
+        int x = 0;
+        int y = 0;
+
+        private void goButton_Click(object sender, EventArgs e)
+        {
+            // assigns random index of compChoices array to variable
+            compChoiceIndex = rand.Next(compChoices.Length);
+
+            // calls methods
+            DisplayPlayersChoices(compChoiceIndex);
+            WinningColor();
+
+
+            // displays the scores of each player
+            while (x < 3 || y < 3)
+            {
+                if (IsWinner(compChoiceIndex))  // if user wins, score is incremented by 1
+                { 
+                    x++;
+                    yourScoreOutput.Text = Convert.ToString(x);
+                    
+                }
+                else if (computerWins(compChoiceIndex)) // if computer wins, score is incremented by 1
+                {
+                    y++;
+                    computerScoreOutput.Text = Convert.ToString(y);
+                    
+                }
+                break;
+            }
+
+
+            // if user reaches a score of 3 first, a message displays user wins and the form is reset
+            if (x == 3)
+            {
+                MessageBox.Show("You Win!");
+                resetForm();
+            }
+
+            // if computer reaches a score of 3 first, a message is displayed and the form is reset
+            if (y == 3)
+            {
+                MessageBox.Show("The computer wins this time.");
+                resetForm();
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        // Method takes the index value of randomly assigned computer choice for either rock, paper, or scissors
+        // 0 -> "rock"  1 -> "paper"    2 -> "scissors"
+        // compares index value to user's selected radio button
+        // returns true if user wins round. Returns false if computer wins round or there is a tie
+        Boolean IsWinner(int index)
+        {
+            if (index == 0 && scissorsRadioButton.Checked)
+            {
+                return false;
+            }
+
+            if (index == 0 && paperRadioButton.Checked)
+            {
+                return true;
+            }
+
+            if (index == 0 && rockRadioButton.Checked)
+            {
+                return false;
+            }
+
+            if (index == 1 && scissorsRadioButton.Checked)
+            {
+                return true;
+            }
+
+            if (index == 1 && paperRadioButton.Checked)
+            {
+                return false;
+            }
+
+            if (index == 1 && rockRadioButton.Checked)
+            {
+                return false;
+            }
+
+            if (index == 2 && scissorsRadioButton.Checked)
+            {
+                return false;
+            }
+
+            if (index == 2 && paperRadioButton.Checked)
+            {
+                return false;
+            }
+
+            else
+            {
+                return true;
+            }
+        }
+
+
+        // Method takes the index value of randomly assigned computer choice for either rock, paper, or scissors
+        // 0 -> "rock"  1 -> "paper"    2 -> "scissors"
+        // compares index value to user's selected radio button
+        // returns true if computer wins round. Returns false if user wins round or there is a tie
+        Boolean computerWins(int index)
+        {
+            if (index == 0 && scissorsRadioButton.Checked)
+            {
+                return true;
+            }
+
+            if (index == 0 && paperRadioButton.Checked)
+            {
+                return false;
+            }
+
+            if (index == 0 && rockRadioButton.Checked)
+            {
+                return false;
+            }
+
+            if (index == 1 && scissorsRadioButton.Checked)
+            {
+                return false;
+            }
+
+            if (index == 1 && paperRadioButton.Checked)
+            {
+                return false;
+            }
+
+            if (index == 1 && rockRadioButton.Checked)
+            {
+                return true;
+            }
+
+            if (index == 2 && scissorsRadioButton.Checked)
+            {
+                return false;
+            }
+
+            if (index == 2 && paperRadioButton.Checked)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+
+
+
+        // Displays player's choices
+        void DisplayPlayersChoices(int compChoiceIndex)
+        {
+            // computer selection
+            computerChoice.Text = compChoices[compChoiceIndex];
+            compChoiceLabel.Visible = true;
+            computerChoice.Visible = true;
+
+            //  user selection
+            if (rockRadioButton.Checked)
+            {
+                yourChoice.Text = "Rock";
+            }
+
+            if (paperRadioButton.Checked)
+            {
+                yourChoice.Text = "Paper";
+            }
+
+            if (scissorsRadioButton.Checked)
+            {
+                yourChoice.Text = "Scissors";
+            }
+
+            yourChoiceLabel.Visible = true;
+            yourChoice.Visible = true;
+
+            // hides the "please make a seletion" label
+            makeSelectionLabel.Visible = false;
+        }
+
+
+        // Changes the color of the winner selection to green, losing selection to red, and tie to blue
+        void WinningColor()
+        {
+            if (IsWinner(compChoiceIndex))
+            {
+                yourChoice.ForeColor = System.Drawing.Color.Green;
+                computerChoice.ForeColor = System.Drawing.Color.Red;
+            }
+
+            else if (computerWins(compChoiceIndex))
+            {
+                yourChoice.ForeColor = System.Drawing.Color.Red;
+                computerChoice.ForeColor = System.Drawing.Color.Green;
+            }
+
+            else
+            {
+                yourChoice.ForeColor = System.Drawing.Color.Blue;
+                computerChoice.ForeColor = System.Drawing.Color.Blue;
+            }
+        }
+
+        // Method resets form
+        void resetForm()
+        {
+            x = 0;
+            y = 0;
+
+            yourScoreOutput.Text = "";
+            computerScoreOutput.Text = "";
+
+            yourChoiceLabel.Visible = false;
+            yourChoice.Visible = false;
+
+            compChoiceLabel.Visible = false;
+            computerChoice.Visible = false;
+
+            makeSelectionLabel.Visible = true;
+
+            rockRadioButton.Checked = true;
+            paperRadioButton.Checked = false;
+            scissorsRadioButton.Checked = false;
+        }
+
+
+    }
+
+
+}
